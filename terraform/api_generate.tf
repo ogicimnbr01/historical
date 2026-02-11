727b69306327cebf0789b8b5b83bb4bbf170ebb6
ogicimnbr01 <ogicimnbr01@users.noreply.github.com>
2026-02-11 21:58:04 +0300

    Tarihsel doğruluk arttırıldı.

--- terraform/api_generate.tf
+++ terraform/api_generate.tf
@@ -123,6 +123,23 @@ resource "aws_api_gateway_integration" "jobs_id_get" {
   uri                     = aws_lambda_function.admin_api.invoke_arn
 }
 
+resource "aws_api_gateway_method" "jobs_id_delete" {
+  rest_api_id      = aws_api_gateway_rest_api.admin_api.id
+  resource_id      = aws_api_gateway_resource.jobs_id.id
+  http_method      = "DELETE"
+  authorization    = "NONE"
+  api_key_required = true
+}
+
+resource "aws_api_gateway_integration" "jobs_id_delete" {
+  rest_api_id             = aws_api_gateway_rest_api.admin_api.id
+  resource_id             = aws_api_gateway_resource.jobs_id.id
+  http_method             = aws_api_gateway_method.jobs_id_delete.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.admin_api.invoke_arn
+}
+
 # /jobs OPTIONS (CORS)
 resource "aws_api_gateway_method" "jobs_options" {
   rest_api_id   = aws_api_gateway_rest_api.admin_api.id
@@ -202,7 +219,7 @@ resource "aws_api_gateway_integration_response" "jobs_id_options" {
   status_code = aws_api_gateway_method_response.jobs_id_options.status_code
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Api-Key,Authorization'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,DELETE,OPTIONS'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 }
